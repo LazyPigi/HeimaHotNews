2e7745607db6223aa82d71115059ed228935d2db
Will-Z-fighting <53569286+Will-Z-fighting@users.noreply.github.com>
2019-09-22 19:45:58 +0800

    初始样式

--- src/main.js
+++ src/main.js
@@ -2,29 +2,18 @@ import Vue from "vue";
 
 // 路由：1.导入路由构造函数
 import VueRouter from "vue-router";
-// 导入vant-ui组件
-import Vant from 'vant';
-// 导入axios
-import axios from "axios";
 
 // 导入组件
 import App from "@/App";
 import Login from "@/pages/Login";
-import Register from "@/pages/Register";
 
 // 在.vue文件中要使用router-link或者router-view.需要注册下插件
 Vue.use(VueRouter);
-Vue.use(Vant);
 
-// 把axios挂载到原型
-Vue.prototype.$axios = axios; // this.$axios
-// 基准路径，以后每次请求都会自动在前面加上该路径
-axios.defaults.baseURL = "http://localhost:3000";
 
 // 路由：2.创建路由配置
 const routes = [
     { path: "/login", component: Login},
-    { path: "/register", component: Register}
 ]
 
 // 路由：3.创建对象
