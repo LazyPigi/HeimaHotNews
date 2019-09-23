f5534e13fa39d1e0bad925288cfc2ddeafee64fc
Will-Z-fighting <53569286+Will-Z-fighting@users.noreply.github.com>
2019-09-23 15:43:22 +0800

    输入框双向数据绑定

--- src/components/AuthButton.vue
+++ src/components/AuthButton.vue
@@ -1,32 +1,9 @@
 <template>
-  <div 
-  class="button" 
-  @click="handleClick">
-    {{text}}
-  </div>
 </template>
 
 <script>
-export default {
-    props: ["text"],
-    methods: {
-        handleClick(event){
-            this.$emit("click", event)
-        }
-    }
-}
+export default {};
 </script>
 
-<style scoped lang="less">
-    .button{
-        width:100%;
-        height: 48 / 360 * 100vw;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        background:#d81e06;
-        color:#fff; 
-        border-radius: 50px;
-        font-style: 20px;
-    }
+<style>
 </style>
