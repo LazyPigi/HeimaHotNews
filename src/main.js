7ebae70faf8423821360f3bf47f25e26d7a3240a
Will-Z-fighting <53569286+Will-Z-fighting@users.noreply.github.com>
2019-09-26 19:56:53 +0800

    跟帖的布局

--- src/main.js
+++ src/main.js
@@ -17,6 +17,7 @@ import Register from "@/pages/Register";
 import Personal from "@/pages/Personal";
 import EditProfile from "@/pages/EditProfile";
 import UserFollow from "@/pages/UserFollow";
+import UserComment from "@/pages/UserComment";
 
 // 在.vue文件中要使用router-link或者router-view.需要注册下插件
 Vue.use(VueRouter);
@@ -35,7 +36,8 @@ const routes = [
     { path: "/register", component: Register },
     { path: "/personal", component: Personal },
     { path: "/edit_profile", component: EditProfile },
-    { path: "/user_follow",  component: UserFollow }
+    { path: "/user_follow",  component: UserFollow },
+    { path: "/user_comment",  component: UserComment },
 
 ]
 
---
+++ src/pages/UserComment.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <HeaderNormal title="我的跟帖"/>
+
+        <!-- 评论的列表 -->
+        <div class="comment-item">
+            <div class="time">2019-9-25 10:10</div>
+
+            <!-- 当前的评论回复的评论 -->
+            <div class="parent">
+                <div class="parent-title">
+                    @: 瑞强
+                </div>
+            
+                <div class="parent-content">
+                    晚饭哪里吃？
+                </div>
+            </div>
+        
+
+            <!-- 个人评论的内容 -->
+            <div class="content">
+                庆和？
+            </div>
+
+            <div class="article-link">
+                <router-link to="#">
+                    原文：又来到，天亮的泥沼，又想到一起看日出，你用力的抱着我，说怕丢了我，我承诺，一直看到老；爱情像烟灰，不经意灰飞。
+                </router-link>
+                <span class="iconfont iconjiantou1"></span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+// 导入头部
+import HeaderNormal from "@/components/HeaderNormal";
+export default {
+    components: {
+        HeaderNormal
+    }
+}
+</script>
+
+<style>
+
+</style>
