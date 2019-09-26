374ce47b7649f144ae65dc9867940c06a4c44b4e
Will-Z-fighting <53569286+Will-Z-fighting@users.noreply.github.com>
2019-09-26 20:32:42 +0800

    用户评论回复的布局

--- src/pages/UserComment.vue
+++ src/pages/UserComment.vue
@@ -23,12 +23,12 @@
                 庆和？
             </div>
 
-            <div class="article-link">
-                <router-link to="#">
-                    原文：又来到，天亮的泥沼，又想到一起看日出，你用力的抱着我，说怕丢了我，我承诺，一直看到老；爱情像烟灰，不经意灰飞。
-                </router-link>
+            <router-link to="#" class="article-link">
+                <p>
+                原文：又来到，天亮的泥沼，又想到一起看日出，你用力的抱着我，说怕丢了我，我承诺，一直看到老；爱情像烟灰，不经意灰飞。
+                </p>
                 <span class="iconfont iconjiantou1"></span>
-            </div>
+            </router-link>
         </div>
     </div>
 </template>
@@ -44,6 +44,46 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang="less">
+.comment-item {
+    padding: 10px;
+    font-size: 14px;
+    line-height: 1.8;
+    border-bottom: 1px #ccc solid;
 
+    .time {
+        font-size: 12px;
+        color: #999;
+        margin-bottom: 5px;
+    }
+
+    .parent {
+        background: #eee;
+        padding: 10px;
+        margin-bottom: 5px;
+
+        .parent-title {
+            font-size: 12px;
+            color: #999;
+        }
+    }
+
+    .content {
+        margin-bottom: 5px;
+    }
+
+    .article-link {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        font-size: 12px;
+        color: #999;
+
+        p {
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+        }
+    }
+}
 </style>
